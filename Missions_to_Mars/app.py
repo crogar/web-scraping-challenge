8f12037580b6e33d7ae1fc225b22495958a07408
crogar <58400421+crogar@users.noreply.github.com>
2021-04-10 22:06:16 -0400

    Update app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,1 +1,17 @@
 from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Set route
+@app.route('/')
+def index():
+    # Store the entire team collection in a list
+    teams = list(db.team.find())
+    print(teams)
+
+    # Return the template with the teams list passed in
+    return render_template('index.html', teams=teams)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
