cdb81c2b5a998754bf1e67ee628de7f09c41bc8a
crogar <58400421+crogar@users.noreply.github.com>
2021-04-10 23:09:38 -0400

    mongo queries fixed

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,3 +1,4 @@
+from re import template
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
@@ -5,16 +6,15 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_mars")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_mars")  # Creating a db named mission_mars if it doesn't exist already
 
 
 # Set route to render index.html
 @app.route('/')
 def index():
     # Find one record of data from the mongo database
-    mars_data = mongo.db.mars_data.find()
+    mars_data = mongo.db.mars_data.find_one()   # since we are only using one Document in the MongoDb, well use find_one() method
     # Return template and data
-    print(mars_data)
     return render_template("index.html", mars_mission=mars_data)
 
 
@@ -24,10 +24,8 @@ def scrape():
 
     # Run the scrape function
     mars_data = scrape_mars.scrape()
-
     # Update the Mongo database using update and upsert=True
     mongo.db.mars_data.update({}, mars_data, upsert=True)
-
     # Redirect back to home page
     return redirect("/")
 
