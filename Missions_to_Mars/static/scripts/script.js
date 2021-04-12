2be7f57cd38339beb9f8b7893f91960c87426de3
crogar <58400421+crogar@users.noreply.github.com>
2021-04-11 20:09:52 -0400

    star-background

--- Missions_to_Mars/static/scripts/script.js
+++ Missions_to_Mars/static/scripts/script.js
@@ -1,11 +1,21 @@
-function resize()
+function wind_load()
 {
     var heights = window.innerWidth;
-    document.getElementsByClassName("stars")[0].style.height = (heights) + "px";
-    document.getElementsByClassName("twinkling")[0].style.height = (heights) + "px";
+    document.getElementsByClassName("stars")[0].style.height = (heights*0.75) + "px";
+    document.getElementsByClassName("twinkling")[0].style.height = (heights*0.75) + "px";
     console.log(heights);
 }
 
-window.onload = function() {
-    resize();
+
+function reportWindowSize() {
+    var heights = window.innerWidth;
+    document.getElementsByClassName("stars")[0].style.height = (heights*1.2) + "px";
+    document.getElementsByClassName("twinkling")[0].style.height = (heights*1.2) + "px";
+    console.log("resized");
+  }
+  
+  window.onload = function() {
+    wind_load();
 };
+
+  // window.addEventListener('resize', reportWindowSize);
